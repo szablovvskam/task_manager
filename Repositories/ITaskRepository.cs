b1689294781d2cc6ffce6dd4512d2b6e11d707ce
Czosnek <martaszablowska21@gmail.com>
2021-02-27 23:23:32 +0100

    First commit

---
+++ Repositories/ITaskRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TaskManager.Models;
+
+namespace TaskManager.Repositories
+{
+    public interface ITaskRepository
+    {
+        TaskModel Get(int taskId);
+        IQueryable<TaskModel> GetAllActive();
+        void Add(TaskModel task);
+        void Update(int taskId, TaskModel task);
+        void Delete(int taskId);
+
+    }
+}
